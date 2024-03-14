1334417374e5b64133ca3962d822182d1364ae13
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-03-14 13:58:44 +1100

    implemented code plotting average lookup time by data structure

--- speed_scripts.py
+++ speed_scripts.py
@@ -1,85 +1,82 @@
 import time
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-#do nothing n times, returns time taken in ns
-def loop_test(n : int) -> int:
-    start_time_ns : int = time.perf_counter_ns()
-    for i in range(n):
-        pass
-    end_time_ns : int = time.perf_counter_ns()
-    return (end_time_ns-start_time_ns)
-
-#build a list of length n, returns time taken in ns
-def list_build_test(n : int) -> int:
+def get_lookup_times(n : int, k : int) -> tuple[np.ndarray,np.ndarray,np.ndarray,np.ndarray]:
+    random_times_ns = np.zeros(n,dtype=float)
+    list_times_ns = np.zeros(n,dtype=float)
+    set_times_ns = np.zeros(n,dtype=float)
+    dict_times_ns = np.zeros(n,dtype=float)
     our_list : list[int] = []
-    start_time_ns : int = time.perf_counter_ns()
-    for i in range(n):
-        our_list.append(i)
-    end_time_ns : int = time.perf_counter_ns()
-    return (end_time_ns-start_time_ns)
-
-#build a set of size n, return time taken in ns
-def set_build_test(n : int) -> int:
     our_set : set[int] = set()
-    start_time_ns : int = time.perf_counter_ns()
+    our_dict : dict[int,bool] = {}
     for i in range(n):
+        our_list.append(i)
         our_set.add(i)
-    end_time_ns : int = time.perf_counter_ns()
-    return (end_time_ns-start_time_ns)
-
-#build a dict of size n, return time taken in ns
-def dict_build_test(n : int) -> int:
-    our_dict : dict[int,bool] = {}
-    start_time_ns : int = time.perf_counter_ns()
-    for i in range(n):
         our_dict[i] = False
-    end_time_ns : int = time.perf_counter_ns()
-    return (end_time_ns-start_time_ns)
+        random_time = random_find_time_per_call(our_list,k)
+        list_time = list_find_time_per_call(our_list,k)
+        set_time = set_find_time_per_call(our_set,k)
+        dict_time = dict_find_time_per_call(our_dict,k)
+        random_times_ns[i] = random_time
+        list_times_ns[i] = list_time
+        set_times_ns[i] = set_time
+        dict_times_ns[i] = dict_time
 
-#test to see how quickly we can generate k random integers in range n, return time taken in ns
-def random_gen_test(n : int, k : int) -> int:
+    return random_times_ns,list_times_ns,set_times_ns,dict_times_ns
+
+def plot_lookup_times(n : int, k : int) -> None:
+    random_times_ns,list_times_ns,set_times_ns,dict_times_ns = get_lookup_times(n,k)
+    x_data = np.arange(n)
+    #plot the data
+    fig = plt.figure()
+    plt.plot(x_data,random_times_ns,'r',label="random")
+    plt.plot(x_data,list_times_ns,'b',label="list")
+    plt.plot(x_data,set_times_ns,'g',label="set")
+    plt.plot(x_data,dict_times_ns,'y',label="dict")
+    plt.legend(loc="upper right")
+    plt.xlabel("size ")
+    plt.ylabel("lookup time (ns)")
+    plt.show(block=False)
+
+#determine average time taken (in ns) to generate a random integer in a list, using k samples to get the average
+def random_find_time_per_call(test_list : list[int],k : int) -> float:
+    list_length = len(test_list)
     start_time_ns : int = time.perf_counter_ns()
     for i in range(k):
-        random_num = random.randrange(n)
+        random_num = random.randrange(list_length)
     end_time_ns : int = time.perf_counter_ns()
-    return (end_time_ns-start_time_ns)
+    return (end_time_ns-start_time_ns)/k
 
-#test to see how quickly we can lookup k random integers in a list of length n, return time taken in ns
-def list_access_test(n : int, k : int) -> int:
-    #create the list
-    our_list : list[int] = []
-    for i in range(n):
-        our_list.append(i)
+#determine average time taken (in ns) to find a random integer in a list, using k samples to get the average
+def list_find_time_per_call(test_list : list[int],k : int) -> float:
+    list_length = len(test_list)
     start_time_ns : int = time.perf_counter_ns()
     for i in range(k):
-        random_num = random.randrange(n)
-        _ = our_list.index(random_num)
+        random_num = random.randrange(list_length)
+        _ = test_list.index(random_num)
     end_time_ns : int = time.perf_counter_ns()
-    return (end_time_ns-start_time_ns)
+    return (end_time_ns-start_time_ns)/k
 
-def list_access_test(n : int, k : int) -> int:
-    #create the set
-    our_list : list[int] = []
-    for i in range(n):
-        our_list.append(i)
+#determine average time taken (in ns) to find a random integer in a set, using k samples to get the average
+def set_find_time_per_call(test_set : set[int],k : int) -> float:
+    set_size = len(test_set)
     start_time_ns : int = time.perf_counter_ns()
     for i in range(k):
-        random_num = random.randrange(n)
-        _ = our_list.index(random_num)
+        random_num = random.randrange(set_size)
+        _ = random_num in test_set
     end_time_ns : int = time.perf_counter_ns()
-    return (end_time_ns-start_time_ns)
+    return (end_time_ns-start_time_ns)/k
 
-def dict_access_test(n : int, k : int) -> int:
-    #create the dictionary
-    our_dict : dict[int,bool] = {}
-    for i in range(n):
-        our_dict[i] = False
+#determine average time taken (in ns) to find a random integer in a set, using k samples to get the average
+def dict_find_time_per_call(test_dict : dict[int],k : int) -> float:
+    set_size = len(test_dict)
     start_time_ns : int = time.perf_counter_ns()
     for i in range(k):
-        random_num = random.randrange(n)
-        _ = our_dict[random_num]
+        random_num = random.randrange(set_size)
+        _ = test_dict[random_num]
     end_time_ns : int = time.perf_counter_ns()
-    return (end_time_ns-start_time_ns)
+    return (end_time_ns-start_time_ns)/k        
+
 
